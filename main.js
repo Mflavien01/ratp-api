95a0533f3d87d8e760fe25c828a196951482e726
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-18 15:16:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var response = JSON.parse(this.responseText);
-		console.log(endpoint);
+		console.log(this.responseText);
 	}
 
 };
