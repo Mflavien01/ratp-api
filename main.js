46f22611ed133b451b6dc871d450bd53d669ba53
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-18 16:09:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,20 @@ xhr.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var response = JSON.parse(this.responseText);
 		var i=0;
+		var j=1;
+		var k=1;
 		if (response.result.schedules[i].message[6]=="t" || response.result.schedules[i].message[5]=="v"){
 			i++;
 		}
+		if (response.result.schedules[i+j].message[6]=="t" || response.result.schedules[i+j].message[5]=="v"){
+			j++;
+		}
+		if (response.result.schedules[i+j+k].message[6]=="t" || response.result.schedules[i+j+k].message[5]=="v"){
+			k++;
+		}
 		document.getElementById("horaire1").innerHTML= "".concat("Prochain passage à : ", response.result.schedules[i].message);
-		document.getElementById("horaire2").innerHTML="".concat("2ème passage à : ", response.result.schedules[i+1].message);
-		document.getElementById("horaire3").innerHTML="".concat("3ème passage à : ", response.result.schedules[i+2].message);
+		document.getElementById("horaire2").innerHTML="".concat("2ème passage à : ", response.result.schedules[i+j].message);
+		document.getElementById("horaire3").innerHTML="".concat("3ème passage à : ", response.result.schedules[i+j+k].message);
 	}
 
 };
