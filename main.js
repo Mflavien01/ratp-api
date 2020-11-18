46ebc034435402e9ae7d3051ad358fc999f0fd38
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-11-18 21:49:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,13 +35,13 @@ xhr.send();
 
 if (window.screen.width > window.screen.height){
 	document.body.style.backgroundImage = "url(train.jpg)";
-	document.getElementById("content").style.display: "";
-	document.getElementById("content").style.align-items:"";
-	document.getElementById("content").style.justify-content: "";
+	document.getElementById("content").style.display="";
+	document.getElementById("content").style.align-items="";
+	document.getElementById("content").style.justify-content="";
 } else{
 	document.body.style.backgroundImage = "url(train_tel.jpg)";
-	document.getElementById("content").style.display: flex;
-	document.getElementById("content").style.align-items:center;
-	document.getElementById("content").style.justify-content: space-between;
+	document.getElementById("content").style.display=flex;
+	document.getElementById("content").style.align-items=center;
+	document.getElementById("content").style.justify-content=space-between;
 }	
 })
