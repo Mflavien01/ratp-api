88f616c6d15e7a8b34072503125fc29becec1f70
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-18 15:59:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var response = JSON.parse(this.responseText);
-		document.getElementById("horaire1").innerHTML= "".concat("Prochain passage à :", response.result.schedules[0].message);
-		document.getElementById("horaire1").innerHTML= "".concat("Prochain passage à :", response.result.schedules[1].message);
-		document.getElementById("horaire1").innerHTML= "".concat("Prochain passage à :", response.result.schedules[2].message);
+		var i=0;
+		if (response.result.schedules[i].message="Train terminus V.A" || response.result.schedules[i].message="Sans voyageurs V.A"){
+			i++;
+		}
+		document.getElementById("horaire1").innerHTML=response.result.schedules[i].message;
 	}
 
 };
