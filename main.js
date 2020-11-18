367ef2e21f1a0f7321747a2678459ff052e23bdc
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-11-18 14:56:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+var list = [];
+var list2 = [];
+var taille=0;
+var taille2=0;
+var mode=0;
+var x=setInterval(function (){
+  var direction = document.getElementById("direction").value;
+  var gare = document.getElementById("gare").value;
 
+  if (direction=="aero"){
+    if (gare=="mas"){ 
+    }
+    if (gare=="ors"){ 
+    }
+    if (gare=="gif"){ 
+    }
+  }
+  if (direction=="strem"){
+    if (gare=="mas"){ 
+    }
+    if (gare=="ors"){ 
+    }
+    if (gare=="gif"){ 
+    }
+  }  
+  
+})
