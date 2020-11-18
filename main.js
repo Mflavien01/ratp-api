b2b821f61f8c3423e86557fb726d68f1bebc07bd
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-11-18 21:29:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ xhr.send();
 },1000)
 
 if (window.screen.width > window.screen.height){
-			document.body.style.backgroundImage = "url(train.jpg)";
-		} else{
-			document.body.style.backgroundImage = "url(train_tel.jpg)";
+	document.body.style.backgroundImage = "url(train.jpg)";
+} else{
+	document.body.style.backgroundImage = "url(train_tel.jpg)";
+}
