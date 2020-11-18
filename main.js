e92292735d315d5d7a8fcc031b80470dc85a5732
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2020-11-18 15:49:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-var list = [];
-var list2 = [];
-var taille=0;
-var taille2=0;
-var mode=0;
 var x = setInterval(function (){
   var direction = document.getElementById("direction").value;
   var gare = document.getElementById("gare").value;
