66288b72fc4f580218ce2c2cffd977c5c89d8f97
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2020-11-20 11:15:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var i=0;
 var j=1;
 
-/*if (localStorage.getItem("direction") === null){
+if (localStorage.getItem("direction") === null){
 	var direction = document.getElementById("direction").value;
  	var gare = document.getElementById("gare").value;
 }else {
-	//document.getElementById("direction").value=localStorage.getItem("direction");
-	//document.getElementById("gare").value=localStorage.getItem("gare");
-}*/
+	document.getElementById(localStorage.getItem("direction")).selected="selected";
+	document.getElementById(localStorage.getItem("gare")).selected="selected";
+}
 
 var x = setInterval(function (){
 	if (innerHeight>800){
